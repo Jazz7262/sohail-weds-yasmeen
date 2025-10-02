1f3c412fa16495800bac398e58d24d9051871c76
Jazz7262 <116513312+Jazz7262@users.noreply.github.com>
2025-10-02 19:30:00 +0530

    Update InviteNote.jsx

--- src/components/InviteNote.jsx
+++ src/components/InviteNote.jsx
@@ -6,22 +6,22 @@ function InviteNote() {
       <section id="invite-note">
           <div className="invite-note-wrapper">
               <h5>An Invitation from:</h5>
-              <h2>Mrs. & Mr. S.P. Nisar Ahmed Sahab</h2>
+              <h2>Mrs. & Mr. G. Khaleel Sahab</h2>
 
               {/* <h5 className="address mb-3">
                     Proprietor: Mohammed Ali Jewellers,
                     <br /> Shroff Bazaar, Adoni.
                 </h5> */}
               <h5 className="address mb-3">
-                  H.NO.: 1/541, NGO's Colony,
+                  H.NO.: 1/670, Khooni Mohallah,
                   <br /> Adoni, Kurnool Dist.,
                   <br /> Andhra Pradesh - 518 301.
               </h5>
               <p className="address">
                   Contact: <br />
-                  {/* +91 83284 22208,
-                  <br /> */}
-                  +91 80084 56560.
+                  +91 73966 62484,
+                  <br />
+                  +91 93986 23226.
               </p>
           </div>
       </section>
